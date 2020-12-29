c0a2f69f19b077f5dc0849051542399dd480003c
Hannes Rüger <hannesrueger@gmx.de>
2020-12-29 11:23:44 +0100

    added feature core src template

---
+++ src/index.ts
@@ -0,0 +1,36 @@
+import { Feature, FeatureSetting, ZoneConfig, SelectionItem } from "@makeproaudio/glue-feature-tools";
+import {  } from "@makeproaudio/glue-feature-tools";
+import { Stack } from "@makeproaudio/makehaus-nodered-lib";
+
+export class MyFeature implements Feature {
+    public zones: ZoneConfig[];
+
+    public init?(): void {
+        //
+    }
+
+    public exit?(): void {
+        //
+    }
+
+    public takeStacksForZone(zoneConfig: ZoneConfig, stacks: Map<number, Stack>): void {
+        //
+    }
+
+    public removeZone(zoneConfig: ZoneConfig, stacks: Map<number, Stack>): void {
+        //
+    }
+
+    public takeSelectorForZone?(zoneConfig: ZoneConfig, selector: any): void {
+        //
+    }
+
+    public getSelectionItems?(zoneConfig: ZoneConfig): SelectionItem[] {
+        return [];
+    }
+
+    public onSettingsChange?(settings: FeatureSetting): void {
+        //
+    }
+
+}
